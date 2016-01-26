aaba8ba14356218c13c59ee2fb27fb75117e3e19
Isabel Cabezas Martín <isabelcabezasm@outlook.com>
2016-01-26 13:04:33 +0100

    Connection Control added

---
+++ Lego.EV3.WPF2/Controls/ConnectionControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Lego.EV3.WPF2
+{
+
+    /// <summary>
+    /// Interaction logic for ConnectionControl.xaml
+    /// </summary>
+    public partial class ConnectionControl : UserControl
+    {
+        public event RoutedEventHandler ConnectToBrick;
+
+        public ConnectionControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ConnectToBrick != null)
+                ConnectToBrick.Invoke(sender, e);
+        }
+
+        public ConnectionType GetConnectionType()
+        {
+            var conTypeString = ConnectionTypeStyle.SelectedItem.ToString();
+            ConnectionType conType;
+
+            Enum.TryParse(conTypeString, true, out conType);
+
+            return conType;
+        }
+
+        public string GetIpAddress()
+        {
+            return IpAddress.Text;
+        }
+
+        public string GetComportNumber()
+        {
+            return "COM" + int.Parse(ComportNumber.Text);
+        }
+    }
+}
