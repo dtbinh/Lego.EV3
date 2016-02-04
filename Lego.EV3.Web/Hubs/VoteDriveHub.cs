693df5d1d20b8006c3b89ecb2541d22ee16d75c6
Isabel Cabezas Martín <isabelcabezasm@outlook.com>
2016-02-04 17:08:01 +0100

    SignalR "first commit" fixed

--- Lego.EV3.Web/Hubs/VoteDriveHub.cs
+++ Lego.EV3.Web/Hubs/VoteDriveHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNet.SignalR;
 using System.Diagnostics;
 using Lego.EV3.Shared;
 
@@ -10,7 +13,25 @@ namespace Lego.EV3.Web.Hubs
         public void SendDriveCommand(DriveCommand command)
         {
             Trace.TraceInformation("SendDriveCommand: " + command.ToString());
-     
+
+            //call to BeAPI
+            ////using (var client = new HttpClient())
+            ////{
+            ////    client.BaseAddress = new Uri("http://legoev3api.azurewebsites.net/");
+            ////    client.DefaultRequestHeaders.Accept.Clear();
+            ////    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            ////    // New code:
+            ////    HttpResponseMessage response = await client.PostAsJsonAsync("api/values", command.ToString());
+            ////    if (response.IsSuccessStatusCode)
+            ////    {
+            ////        var a = response.Headers.Location;
+            ////    }
+
+
+            ////}
+
+
         }
 
     }
