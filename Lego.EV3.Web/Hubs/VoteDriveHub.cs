40c8f9b15745ca14ddfcad4c0f65d8fb9828b905
Isabel Cabezas Martín <isabelcabezasm@outlook.com>
2016-02-04 16:45:30 +0100

    SignalR Added

---
+++ Lego.EV3.Web/Hubs/VoteDriveHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
+using Lego.EV3.Shared;
+
+namespace Lego.EV3.Web.Hubs
+{
+    public class VoteDriveHub : Hub
+    {
+
+        public void SendDriveCommand(DriveCommand command)
+        {
+            Trace.TraceInformation("SendDriveCommand: " + command.ToString());
+     
+        }
+
+    }
+}
---
+++ Lego.EV3.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+namespace Lego.EV3.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+} 
