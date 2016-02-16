7497fef184716562777194e7cf71bccbdf349687
Dani <daesgar90@outlook.com>
2016-02-16 14:27:29 +0100

    Added a table showing the votes counting.

--- Lego.EV3.Net.Web/Hubs/VoteDriveHub.cs
+++ Lego.EV3.Net.Web/Hubs/VoteDriveHub.cs
@@ -5,15 +5,35 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using System.Threading.Tasks;
 
 namespace Lego.EV3.Net.Web.Hubs
 {
     public class VoteDriveHub: Hub
     {
 
+        public static Dictionary<DriveCommand, int> CurrentVotesCount = new Dictionary<DriveCommand, int>()
+        {
+            {DriveCommand.Left, 0 },
+            {DriveCommand.Forward, 0 },
+            {DriveCommand.Right, 0 },
+            {DriveCommand.Back, 0 },
+        };
+
         public void SendDriveCommand(DriveCommand command)
         {
+            if(CurrentVotesCount[command] < Int32.MaxValue)
+            {
+                CurrentVotesCount[command]++;
+            }
+            Clients.All.updateVotesCounter(CurrentVotesCount);
             Trace.TraceInformation("SendDriveCommand: " + command.ToString());
+        }
+
+        public override Task OnConnected()
+        {
+            Clients.All.updateVotesCounter(CurrentVotesCount);
+            return base.OnConnected();
         }
 
     }
